0049a72857fe64d234c897bdedbf83c75e3a6fb5
Andrés Carnicero <acarni01@ucm.es>
2021-03-19 11:06:59 +0100

    Componente Roll dentro

---
+++ Project Stroke/Proyecto/components/Roll.cpp
@@ -0,0 +1,27 @@
+#include "Roll.h"
+
+Roll::Roll() :
+	tr_(nullptr) {
+};
+
+Roll::~Roll() {
+	//me follo a tu madre
+}
+
+void Roll::init() {
+
+}
+
+void Roll::update() {
+
+};
+
+float Roll::lerp(float a, float b, float f) {
+
+	//Pongo esto para que no de error de compilacion
+	return 0;
+};
+
+void Roll::accion() {
+
+};
---
+++ Project Stroke/Proyecto/components/Roll.h
@@ -0,0 +1,39 @@
+#pragma once
+// This file is part of the course TPV2@UCM - Samir Genaim
+
+#include "../ecs/Component.h"
+#include "../ecs/Entity.h"
+#include "../sdlutils/InputHandler.h"
+#include <map>
+#include <cmath>
+
+#include "Transform.h"
+
+
+class Roll : public Component {
+public:
+	Roll();
+
+	virtual ~Roll();
+
+	void init() override;
+
+	void update();
+
+	float lerp(float a, float b, float f);
+
+	//Ejecuta la habilidad en funcion del hamster que sea
+	virtual void accion();
+
+	//Devuelve si puede volvera lanzar la habilidad o no
+	bool getCanAttack() { return canAttack; }
+
+	//Setters auxiliares
+	void setCanAttack(bool can) { canAttack = can; }
+	void setTime(float seconds) { timer = seconds; }
+private:
+	Transform* tr_;
+	bool canAttack;
+	float timer; //Contador para ver cada cuanto puede usar una habilidad
+};
+
