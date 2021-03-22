5ed0da59930058b73a4004a45013cf4e68833be3
diecasti <decasti@ucm.es>
2021-03-22 16:46:38 +0100

    HC finiquito en clase

---
+++ Project Stroke/Proyecto/components/ControlHandeler.cpp
@@ -0,0 +1,13 @@
+#include "ControlHandeler.h"
+
+void ControlHandeler::init() {
+
+
+}
+
+
+//el update recibe todos los input de SDL los filtra y envia la respuesta a la clase
+void ControlHandeler::update() {
+
+
+}
