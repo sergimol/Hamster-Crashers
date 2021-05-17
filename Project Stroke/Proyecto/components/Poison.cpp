7d8493ed025ffc074c71fbf93438407f7d2d9039
albemu07 <albemu07@ucm.es>
2021-05-17 16:30:56 +0200

    Animacion de la nube de keta

--- Project Stroke/Proyecto/components/Poison.cpp
+++ Project Stroke/Proyecto/components/Poison.cpp
@@ -6,7 +6,7 @@
 #include "Image.h"
 #include "Cloud.h"
 
-Poison::Poison(int dmg) : Ability(CLOUDCD), dmg_(dmg) {
+Poison::Poison(int dmg) : Ability(2000), dmg_(dmg) {
 };
 
 Poison::~Poison() {
@@ -35,10 +35,17 @@ void Poison::action() {
 	cloud_ = entity_->getMngr()->addEntity();
 
 	cloud_->addComponent<Transform>(
-		tr_->getPos() + Vector2D(tr_->getW() / 2 + x * COFFSETX, tr_->getH() / 2),
-		Vector2D(x, 0.0f) * CBULLETSPEED, 10.0f, 10.0f, 0.0f,1,1);
-
-	cloud_->addComponent<Image>(&sdlutils().images().at("keta"));
+		tr_->getPos() + Vector2D(tr_->getW() / 6 + x * COFFSETX, tr_->getH() / 6),
+		Vector2D(x, 0.0f) * CBULLETSPEED, 134.0f*3, 58.0f*3, 0.0f,1,1);
+	//Texture* tex, int w, int h, int c, int r, Uint32 f, Vector2D sf, int dur
+	cloud_->addComponent<Animator>(&sdlutils().images().at("cloud"),
+		134,
+		58,
+		14,
+		1,
+		60,
+		Vector2D(0, 0),
+		14);
 
 	cloud_->addComponent<Cloud>(dmg_);
 }
