0989f41e68bd0cb9f066401ac7701a6a640a7a4f
javimu06 <javimu06@ucm.es>
2021-05-26 03:22:08 +0200

    Revert "e"
    This reverts commit 600f2953c77f8ae196a62ee4b148086058824458.

--- Project Stroke/Proyecto/components/Transition.cpp
+++ Project Stroke/Proyecto/components/Transition.cpp
@@ -76,7 +76,7 @@ void Transition::fadeIn() {
 		if (subs_ != nullptr && numTReference > 0) {
 			subs_->dialogoStateChange();
 			entity_->getMngr()->getHandler<SoundManager>()->getComponent<SoundManager>()->play("nextPage");
-			entity_->getMngr()->getHandler<SoundManager>()->getComponent<SoundManager>()->play("stronghit");
+			entity_->getMngr()->getHandler<SoundManager>()->getComponent<SoundManager>()->play("transition");
 			numTReference--;
 		}
 	}
